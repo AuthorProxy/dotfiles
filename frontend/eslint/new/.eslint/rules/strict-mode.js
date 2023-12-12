2f193a93a5dfd9cb4fee1089d4dc20d2a93c977c
Alex Kostyukov <alexander@nerdybunny.io>
2023-12-12 21:06:43 +0300

    remove old files and update vscode and git settings

--- frontend/eslint/new/.eslint/rules/strict-mode.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-
-/* Strict Mode: http://eslint.org/docs/rules/#strict-mode */
-module.exports = {
-  rules: {
-    // require or disallow strict mode directives
-    strict: 'error'
-  }
-};
